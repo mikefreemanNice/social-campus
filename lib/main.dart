1c75ca566ed4cbd127748e12aa4dc310c35eaf1e
wujieheng <wujieheng@shuidihuzhu.com>
2019-06-04 17:09:48 +0800

    feat: add page

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,23 @@
+import 'dart:io';
+
 import 'package:fluro/fluro.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:social_campus/router/application.dart';
 import 'package:social_campus/router/router_conf.dart';
 import 'package:social_campus/views/page/main_page.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+  if (Platform.isAndroid) {
+    //设置Android头部的导航栏透明
+    SystemUiOverlayStyle systemUiOverlayStyle =
+        SystemUiOverlayStyle(statusBarColor: Colors.lightBlue[300]);
+    SystemChrome.setSystemUIOverlayStyle(systemUiOverlayStyle);
+  }
+}
 
-const Color ThemeColor = Colors.lightBlue;
+const Color ThemeColor = Color(0xFF4FC3F7);
 
 class MyApp extends StatelessWidget {
   MyApp() {
@@ -25,18 +36,16 @@ class MyApp extends StatelessWidget {
         primaryColor: ThemeColor,
         backgroundColor: Color(0xFFEFEFEF),
         accentColor: Color(0xFF888888),
-        textTheme: TextTheme(
-          //设置Material的默认字体样式
-          body1: TextStyle(color: Color(0xFF888888), fontSize: 16.0),
-        ),
-        iconTheme: IconThemeData(
-          color: ThemeColor,
-          size: 40.0,
-        ),
+        // textTheme: TextTheme(
+        //   //设置Material的默认字体样式
+        //   body1: TextStyle(color: Color(0xFF888888), fontSize: 16.0),
+        // ),
+        // iconTheme: IconThemeData(
+        //   color: ThemeColor,
+        //   size: 40.0,
+        // ),
       ),
-      home: Scaffold(
-        body: welcomePage()
-      ),
+      home: Scaffold(body: welcomePage()),
       onGenerateRoute: Application.router.generator,
     );
   }
